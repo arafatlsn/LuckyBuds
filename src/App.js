ff16d6f497695f84cac775b8459974492058f96e
Arafat37 <arafatsarkar22@gmail.com>
2022-03-26 18:13:24 +0600

    added qna

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Headers/Header';
 import Main from './Components/Main/Main';
+import QnA from './Components/QestionAnswer/QnA';
 
 function App() {
   return (
     <div className='app-div'>
       <Header></Header>
       <Main></Main>
+      <QnA></QnA>
     </div>
   );
 }
---
+++ src/Components/QestionAnswer/QnA.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './QnA.css'
+
+const QnA = () => {
+  return (
+    <div className='qna-parent'>
+      <div className='qna-div'>
+          <h3>Q. How React Works?</h3>
+          <p><mark>Ans: </mark>React is one of the most popular library of JavaScript. React mainly component based front end library. React application is made of multiple components. React create a virtual dom which do compare with dom tree. where is change by user react change only that element and set on dom tree.</p>
+
+          <h3>Q. Props vs State defference?</h3>
+          <p><mark>Ans:</mark></p>
+          <h6><mark>Props:</mark></h6>
+          <p>props only get data from parent. can not pass data to parent.</p>
+          <p>props data read only, can't change data.</p>
+          <p>prpos can be defaulted and validated</p>
+          <h6><mark>State:</mark></h6>
+          <p>Created with component.</p>
+          <p>State can be change.</p>
+          <p>State can not make component reusable</p>
+          <p>State is mutable</p>
+      </div>
+    </div>
+  );
+};
+
+export default QnA;
